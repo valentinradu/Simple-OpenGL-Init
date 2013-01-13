23915a3927feb4a7855cf8a9c7e8bc78d76e8b1f
Valentin Radu <radu.v.valentin@me.com>
2013-01-13 16:32:18 +0000

    Initial Commit

---
+++ SimpleOpenGLInit/SGLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SGLAppDelegate.h
+//  SimpleOpenGLInit
+//
+//  Created by Valentin Radu on 13/01/2013.
+//  Copyright (c) 2013 Valentin Radu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleOpenGLInit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleOpenGLInit
+//
+//  Created by Valentin Radu on 13/01/2013.
+//  Copyright (c) 2013 Valentin Radu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SGLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SGLAppDelegate class]));
+    }
+}
